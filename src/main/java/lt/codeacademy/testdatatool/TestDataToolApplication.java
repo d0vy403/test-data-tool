252d75414618d2bdb84edd6ac73f0be2ff83f1f2
dovy <dovy@code.com>
2025-04-09 18:45:09 +0300

    Initial commit

---
+++ src/main/java/lt/codeacademy/testdatatool/TestDataToolApplication.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.testdatatool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDataToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDataToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/codeacademy/testdatatool/TestDataToolApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.testdatatool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDataToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
